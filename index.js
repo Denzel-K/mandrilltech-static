20f5b2d49b44328b21a39555286711e5a16f8954
Denzel <denzelk741@gmail.com>
2024-08-24 16:42:56 +0300

    Mobile project added

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ fetchData()
     //Render stats dynamically
     let fmOutput = '';
     let webOutput = '';
+    let mobileOutput = '';
 
     for(let i = 0; i < results.length; i++){
       // Format the publishDate using the custom function
@@ -98,10 +99,38 @@ fetchData()
           </div>
         `;
       }
+
+      else if(results[i].category === "mobile"){
+        mobileOutput += /*html*/ `
+          <div class="p-item">
+            <div class="p-image">
+              <img src="images/thumbnails/${results[i].thumbnail}" alt="fm">
+            </div>
+
+            <div class="p-desc">
+              <div class="p_head">
+                <span>${formattedDate}</span>
+
+                <div class="p-links">
+                  <a href="${results[i].repo}" target="_blank">
+                    <img src="images/github.svg" alt="github">
+                  </a>
+                  <a href="${results[i].liveSite}" target="_blank">
+                    <img src="images/download.svg" alt="link-to">
+                  </a>
+                </div>
+              </div>
+
+              <h3>${results[i].title}</h3>
+            </div>
+          </div>
+        `;
+      }
     }
 
     document.querySelector('#fm').innerHTML = fmOutput;
     document.querySelector('#web').innerHTML = webOutput;
+    document.querySelector('#mobile').innerHTML = mobileOutput;
   })
   .catch(err => console.log("Promise rejected: ", err));
 
@@ -180,6 +209,10 @@ function pending(){
   coming_up.classList.remove('hidden');
   full_archive_link.classList.add('hidden');
 }
+function available(){
+  coming_up.classList.add('hidden');
+  full_archive_link.classList.add('hidden');
+}
 
 fm_li.onclick = () => {
   project_title.textContent = "Frontend Mentor";
@@ -188,6 +221,7 @@ fm_li.onclick = () => {
   mobile_li.style = "background: transparent";
   desktop_li.style = "background: transparent";
   
+  document.querySelector('#mobile').classList.add('hidden');
   document.querySelector('#fm').classList.remove('hidden');
   document.querySelector('#web').classList.add('hidden');
   coming_up.classList.add('hidden');
@@ -200,11 +234,11 @@ web_li.onclick = () => {
   mobile_li.style = "background: transparent";
   desktop_li.style = "background: transparent";
   fm_li.style = "background: transparent";
-  
+
+  document.querySelector('#mobile').classList.add('hidden');
   document.querySelector('#web').classList.remove('hidden');
   document.querySelector('#fm').classList.add('hidden');
-  coming_up.classList.add('hidden');
-  full_archive_link.classList.add('hidden');
+  available();
 }
 
 mobile_li.onclick = () => {
@@ -214,9 +248,10 @@ mobile_li.onclick = () => {
   desktop_li.style = "background: transparent";
   fm_li.style = "background: transparent";
 
+  document.querySelector('#mobile').classList.remove('hidden');
   document.querySelector('#fm').classList.add('hidden');
   document.querySelector('#web').classList.add('hidden');
-  pending();
+  available();
 }
 
 desktop_li.onclick = () => {
@@ -226,6 +261,7 @@ desktop_li.onclick = () => {
   mobile_li.style = "background: transparent";
   fm_li.style = "background: transparent";
 
+  document.querySelector('#mobile').classList.add('hidden');
   document.querySelector('#fm').classList.add('hidden');
   document.querySelector('#web').classList.add('hidden');
   pending();
